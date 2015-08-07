db53d8c241d3967003f988280efe569ef27f1e73
Thomas Braun <thomas.braun@byte-physics.de>
2015-08-07 19:00:40 +0200

    Add xop stub creation script from oldstuff repository

---
+++ example-header.h
@@ -0,0 +1,12 @@
+/*
+ * Author:  Thomas Braun, thomas dot braun aeht virtuell minus zuhause dot de
+ * License: GPLv3 or later
+ * Version: 0.12
+ * Date: 4/20/2011
+ *
+ * Purpose: Example file for xop-stub-generator.pl
+*/
+
+variable openFile(string absoluteFilePath, string fileName);
+string getBugReportTemplate();
+THREADSAFE variable getFileName(string *filename);
